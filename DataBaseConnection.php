d724f3b3e19524b9020ceabf50961b78ff922ef1
gronaldo44 <98724929+gronaldo44@users.noreply.github.com>
2022-01-30 20:35:12 -0700

    Update DataBaseConnection.php

--- DataBaseConnection.php
+++ DataBaseConnection.php
@@ -2,7 +2,7 @@
 
 $host = "mysqlcsis2440.cm9qjyuqifrz.us-west-2.rds.amazonaws.com";
 $user = "admin";
-$password = "Qw_1234567891011";
+$password = "password";   // not the real password for obvious reasons. Email me if this is a problem, gronaldo44@gmail.com
 $dbname = "CSIS2440";
 $con = new mysqli($host, $user, $password, $dbname)
         or die('Could not connect to the database server. ' . mysqli_connect_error($con));
